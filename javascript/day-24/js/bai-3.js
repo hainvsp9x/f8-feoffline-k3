bb97c13af3a2d0bfab61b624965943017b1f711d
hai <haisp9x@gmail.com>
2023-09-11 16:40:14 +0700

    Update color hex

--- javascript/day-24/js/bai-3.js
+++ javascript/day-24/js/bai-3.js
@@ -1,5 +1,5 @@
 function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215);
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 console.log(getRandomColor());
 var randomColor = document.querySelector(".random-color");
